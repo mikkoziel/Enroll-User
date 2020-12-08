19f64f263eaf25bfaf6386e2c6ece308a10154a2
Mikołaj Kozieł <mkoziel045@gmail.com>
2020-12-08 03:07:11 +0100

    Changes in routing

--- Enroll-User/src/app/app-routing.module.ts
+++ Enroll-User/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ScheduleDetailsComponent } from './schedule-details/schedule-details.component';
 import { UserPanelComponent } from './user-panel/user-panel.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/user', pathMatch: 'full'},
     { path: 'login', component: LoginComponent },
     { path: 'user', component: UserPanelComponent },
+    { path: 'schedule-details/:id', component: ScheduleDetailsComponent },
   ];
   
   @NgModule({
